ae0f3491d7d2446e09bbd4af72839a7040db018e
Jayson Reis <santosdosreis@gmail.com>
2018-06-24 19:40:33 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,41 +12,45 @@ mod executor;
 use std::fs::File;
 use std::io;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 
 use errors::{Result, ResultExt};
 use executor::Executor;
 
-const PROGRAM: &'static str = "csv-query";
+const PROGRAM: &'static str = "csv_query";
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn process() -> Result<()> {
     let matches = App::new(PROGRAM)
         .version(VERSION)
-        .arg(
-            Arg::with_name("query")
-                .long("query")
-                .short("q")
-                .takes_value(true)
-                .required(true)
-                .help("Query to run over CSV file(s)"),
-        )
-        .arg(
-            Arg::with_name("files")
-                .short("f")
-                .long("files")
-                .takes_value(true)
-                .required(true)
-                .multiple(true)
-                .help("CSV files to work with"),
-        )
-        .arg(
-            Arg::with_name("delimiter")
-                .short("d")
-                .long("delimiter")
-                .takes_value(true)
-                .default_value(";")
-                .help("Delimiter used in your CSV"),
+        .subcommand(
+            SubCommand::with_name("run_query")
+                .about("Run queries over CSV files")
+                .arg(
+                    Arg::with_name("query")
+                        .long("query")
+                        .short("q")
+                        .takes_value(true)
+                        .required(true)
+                        .help("Query to run over CSV file(s)"),
+                )
+                .arg(
+                    Arg::with_name("files")
+                        .short("f")
+                        .long("files")
+                        .takes_value(true)
+                        .required(true)
+                        .multiple(true)
+                        .help("CSV files to work with"),
+                )
+                .arg(
+                    Arg::with_name("delimiter")
+                        .short("d")
+                        .long("delimiter")
+                        .takes_value(true)
+                        .default_value(";")
+                        .help("Delimiter used in your CSV"),
+                ),
         )
         .get_matches();
 
@@ -60,16 +64,21 @@ fn process() -> Result<()> {
         input_buffers.push(io::BufReader::new(f?));
     }
 
-    let delimiter = {
-        let delimiter = matches.value_of("delimiter").unwrap();
-        if delimiter.len() > 1 {
-            bail!("Invalid delimiter {:?}", delimiter);
-        }
-        delimiter.as_bytes()[0]
-    };
+    let delimiter = matches
+        .value_of("delimiter")
+        .and_then(|delimiter| {
+            if delimiter.len() > 1 {
+                bail!("Invalid delimiter {:?}", delimiter);
+            }
+            Ok(delimiter)
+        })
+        .and_then(|delimiter| Ok(delimiter.as_bytes()[0]));
 
-    let mut executor =
-        Executor::with_csv(input_buffers, io::BufWriter::new(io::stdout()), delimiter)?;
+    let mut executor = Executor::with_csv(
+        input_buffers,
+        io::BufWriter::new(io::stdout()),
+        delimiter.unwrap(),
+    )?;
     let query = matches.value_of("query").unwrap();
     executor.print_results(query)?;
     Ok(())
